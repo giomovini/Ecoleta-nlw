16fd7c132abe2f0481019ca04d7b619afda363b8
Vinicius Giomo <vini.giomo@gmail.com>
2020-07-21 14:36:18 -0300

    Primeiro commit

---
+++ public/scripts/into.js
@@ -0,0 +1,65 @@
+//document.write("Hello")
+
+//variaveis, tipos de dados
+//var myvar = "he"
+//const myconst = "he"
+
+//document.write(myconst + myvar)
+
+//string
+//"Isso é uma string"
+//'Isso também é uma string'
+//`Isso é uma string também`
+
+//number
+//const n1 = 1
+//const n2 = 12
+//document.write(n1 + n2)
+ 
+//boulean - true or false
+//const bTrue = true
+//const bFalse = false
+//document.write(bFalse)
+
+//objeto possuem propriedades e funcionalidades
+//const pessoa = {
+  //altura: "1,80m",
+  //idade: 24,
+  //solteiro: true,
+  //correr(){
+    //document.write("executar uma grande lógica de correr")
+//  }
+//}
+ //pessoa.correr()
+
+// Array - vetores
+//const colecaoDeBolinhas = ["blue", "green", 1, {name: "My Name"}]
+
+//document.write(colecaoDeBolinhas[3].name)
+
+
+//Funções - Funcionalidades - Atalhos - Reuso de código
+//function sayMyName(name) {
+  //document.write(name)
+//}
+
+//executar
+//sayMyName("Douglas")
+
+//condicionais
+
+//const notaFinal = 7
+//se
+//if( notaFinal < 5 ) {
+ // document.write("Reprovado")
+  //caminho 1
+//} else  {
+  //contrario de if //caminho 2
+ // document.write("Aprovado")
+//}
+
+//loop - repetições
+
+//for (i = 0; i < 10; i++) { // ++ = jeito curto de falar i = i + 1
+  //document.write(`<p> ${i} </p>`)
+//}
